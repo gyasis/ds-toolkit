5852792c8965c779476afae22c2a199716b42e3d
system1c <gyasi@gyasi.com>
2021-01-30 13:52:27 +0100

    created basicanalysis fuction

---
+++ BasicAnalysis.py
@@ -0,0 +1,46 @@
+def collectNames(dataframe):
+        columns = list(dataframe.columns)
+        return columns 
+       
+
+def collectgraph(dataframe, columns):
+
+    #future feature - ask for a Y variable to drop
+    
+    #Scaling is needed correctly show all variables distribution at the same time -> Scaling prep
+    from sklearn.preprocessing import StandardScaler, MinMaxScaler
+    
+    scaler = StandardScaler()
+    minmax = MinMaxScaler()
+
+
+    tempdf = dataframe.select_dtypes(exclude=['object'])
+    o_length = len(columns)
+    new_length = len(list(tempdf.columns))
+    dropped_columns = o_length - new_length
+
+    #scale data
+    tempdf = scaler.fit_transform(tempdf)
+
+    #warn of dropped columns
+    if (dropped_columns > 0):
+        print(dropped_columns,"columns were dropped")
+        print("try to convert the dropped columns")
+
+    #Print preliminary info of original data frame
+    print(dataframe.info())
+    print(dataframe.isna().sum())
+    print(dataframe.isna().sum() * 100)
+
+    
+    #graphing
+    fig, (ax1) = plt.subplots(ncols=1, figsize=(10, 8))
+    ax1.set_title('Original Distributions')
+    sns.kdeplot(data = tempdf,ax=ax1, legend=True)
+
+    sns.pairplot(dataframe , diag_kind='kde'
+
+    
+def buildgraph(dataframe):
+    columns = collectNames(dataframe)
+    collectgraph(dataframe, columns)
