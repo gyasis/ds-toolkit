041e60aecf87fbfff15cc7213632f32558815f94
gyasis <gyasis@gmail.com>
2021-06-30 22:56:37 +0200

    precheck update

--- custom_modules/precheck.py
+++ custom_modules/precheck.py
@@ -1,25 +1,41 @@
 # %%
-def precheck():
+# %%
+def precheck(x):
 
     import glob
     import os
-
-    files = glob.glob('**', recursive=True)
-    files = reversed(sorted(files, key=os.path.getsize))
-
+    import pandas as pd
+    
+    files = glob.glob((x+'/*'), recursive=True)
+    # files = reversed(sorted(files, key=os.path.getsize))
+   
+    filename = []
+    filesize = []
+    totalsize = 0
     for item in files:
-        size = os.path.getsize(item)
-        if os.path.isfile(item):
-            if (size >= 1048576):
-                print(item + "->\t\t\t\t\t", (os.path.getsize(item) / 1048576),
-                      "MB")
-            elif (size >= 1024):
-                print(item + "->\t\t\t\t\t", (os.path.getsize(item) / 1024),
-                      "KB")
-            else:
-                print(item + "->\t\t\t\t\t", (os.path.getsize(item)), "bytes")
+        try:
+            size = os.path.getsize(item)
+            totalsize += size
+            if os.path.isfile(item):
+                if (size >= 10485760):
+                    # print(item + "->\t\t\t\t\t", (size / 1048576),
+                        # "MB")
+                    filename.append(item)
+                    filesize.append(size / 1048576)
+              
+        except:
+            # print('Warning!! Unrecognizable filetype', item)                   
+            filename.append("WARNING!!! ---->"+item)
+            filesize.append(0)
+            
+    totalsize = totalsize / 1048576
+    filename.append('Total MB: ')
+    filesize.append(totalsize)
+    df = pd.DataFrame(list(zip(filename, filesize)),
+               columns =['File', 'MB'])
+    df = df.sort_values(by=['MB'], ascending=False)
+    return df
 
-precheck()
 # %%
 
 # %%
